f91ca157c3abe366e40448939cee9758fb28620d
MrMo76 <118379930+MrMo76@users.noreply.github.com>
2024-01-20 20:00:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 require("dotenv").config();
-
 const discord = require("discord.js-selfbot-v13"); 
-const client = new discord.Client();
 
-client.on('ready', async () => {
+const express = require("express");
+const app = express();
+
+const ONLINE_USERS = [];
+
+/* Split the tokens with /split/ */
+
+if(!process.env.token) throw Error("[TOKEN] No Token was provided!")
+
+for(let i of process.env.token.split("/split/")) {
+
+const client = new discord.Client({
+  checkUpdate: false
+});
+
+client.on("ready", () => {
+
   console.log(`${client.user.username} is ready!`);
-})
+  ONLINE_USERS.push(client.user.username);
+});
 
 const {userAccount} = require("sphinx-run");
-new userAccount(client, discord).leveling({
-    channel: "1195813914219393217",
+
+const Random = Math.floor(Math.random() * 15) + 10;
+
+  new userAccount(client, discord).leveling({
+    channel: "1197852013568995348",
     randomLetters: false,
-    time: 10000, 
+    time: Random * 1000, 
     type: 'eng',
 });
 
-client.login(process.env.token);
+client.login(i);
+
+}
+
+app.get("/", (req, res) => {
+  res.json({ message: "Success", users: ONLINE_USERS || "None!" });
+});
+
+app.listen(3000, function() {
+  console.log("Express APP is listening on port 3000!");
+});
